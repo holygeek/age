1425223850a25e2a782f15794fd01e0121e88870
Nazri Ramliy <ayiehere@gmail.com>
2013-03-29 08:48:15 +0800

    reimplement in go

---
+++ age.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"flag"
+	"time"
+	"os/exec"
+	"github.com/holygeek/timetext"
+)
+
+func main() {
+	var file bool
+	var noPath bool
+	var since int64
+	var delta int64
+
+	flag.Usage = func() {
+		fmt.Fprintln(os.Stderr, os.Args[0] + " [-h] [-f|-s|-d] [-n] <arg>")
+		flag.PrintDefaults()
+	}
+	flag.BoolVar(&file, "f", false, "Show age of given file")
+	flag.BoolVar(&noPath, "n", false, "Suppress file fullpath output")
+	flag.Int64Var(&delta, "d", -1, "Show age for the given seconds")
+	flag.Int64Var(&since, "s", -1, "Show age since the given seconds from epoch")
+	flag.Parse()
+
+	var age int64
+	if since >= 0 {
+		age = time.Now().Unix() - since
+	} else if delta >= 0 {
+		age = delta
+	} else {
+		if flag.NArg() == 0 {
+			flag.Usage()
+			return
+		}
+
+		for _, arg := range flag.Args() {
+			var filename string
+			if file {
+				filename = arg
+			} else {
+				exe := arg
+				cmd := exec.Command("which", exe)
+				if output, err := cmd.Output(); err != nil {
+					fmt.Fprintln(os.Stderr, err)
+					return
+				} else {
+					filename = string(output[0:len(output) - 1])
+				}
+			}
+
+			if fi, err := os.Stat(filename); err == nil {
+				age = time.Now().Unix() - fi.ModTime().Unix()
+				if ! noPath {
+					fmt.Print(filename + ": ")
+				}
+				fmt.Println(timetext.Duration(age))
+			} else {
+				fmt.Fprintln(os.Stderr, err)
+				return
+			}
+		}
+		return
+	}
+	fmt.Println(timetext.Duration(age))
+}
